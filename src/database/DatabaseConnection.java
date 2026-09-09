8bd5c67ddb984389cc346dff44907669cf440bee
Niharika K Thilak <niharikathilak@gmail.com>
2026-09-09 20:36:36 +0530

    Secure database password configuration

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/scms";
     private static final String USER = "root";
-    private static final String PASSWORD = "MYSQL_PASSWORD";
+    private static final String PASSWORD = System.getenv("SCMS_DB_PASSWORD");
 
     public static Connection getConnection() {
 
